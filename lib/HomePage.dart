72cefcfdc2886bae191ba3456da761a3b58437ef
gisela ramadinawati <gigisgisela2111@gmail.com>
2022-10-24 11:44:59 +0700

    update  navigationbar

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,19 +1,57 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  HomePageState createState() => HomePageState();
-}
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-class HomePageState extends State<HomePage> {
+  static const String _title = 'Flutter Code Sample';
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        leading: Image(
-          image: AssetImage("asset/dewi.png"),
-        ),
+        leading: Image(image: AssetImage('asset/dewi.png')),
+        backgroundColor: Color.fromARGB(255, 65, 129, 226),
         title: Center(
           child: Text(
             "Home",
@@ -26,6 +64,29 @@ class HomePageState extends State<HomePage> {
           ),
         ],
       ),
-    ));
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorite',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_on_sharp),
+            label: 'Maps',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Color.fromARGB(255, 255, 255, 255),
+        backgroundColor: Color.fromARGB(255, 65, 129, 226),
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
