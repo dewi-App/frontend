e139de58bcc93f76242bc8c60a945f9a1cc704c2
nadaakn <nadaa.khoirunnafisah@gmail.com>
2022-10-29 22:22:15 +0700

    added categories

--- lib/users/homepage.dart
+++ lib/users/homepage.dart
@@ -9,7 +9,26 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Home Page')),
+      body:
+          // Column(
+          // children: [
+          Container(
+        alignment: Alignment.topCenter,
+        child: Card(
+          // Card(
+          // Kamu udah tidur yaaa :)
+          // Aku coba cari cara nya dulu siang dilanjut ya
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadiusDirectional.circular(40)),
+          clipBehavior: Clip.antiAlias,
+          child: Image.asset(
+            "assets/hotel.jpeg",
+            fit: BoxFit.cover,
+            width: 700,
+            height: 120,
+          ),
+        ),
+      ),
     );
   }
 }
