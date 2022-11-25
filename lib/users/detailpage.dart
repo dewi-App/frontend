b94bc17014b277a3d5efbc774b04800102c5a385
nadaakn <nadaa.khoirunnafisah@gmail.com>
2022-11-26 00:31:02 +0700

    added icons rating and added detail page

--- lib/users/detailpage.dart
+++ lib/users/detailpage.dart
@@ -9,7 +9,27 @@ class _detailpageState extends State<detailpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Detail Page')),
-    );
+        appBar: AppBar(
+          title: Text('Detail Hotel'),
+        ),
+        body: Center(
+            child: Column(children: <Widget>[
+          Image.asset(
+            'assets/hotel.jpeg',
+            height: 200,
+            width: 1000.0,
+            fit: BoxFit.cover,
+          ),
+          ElevatedButton(
+            child: Text('Lihat Di Maps'),
+            style: ElevatedButton.styleFrom(
+              minimumSize: Size(1000.0, 40),
+              primary: Color.fromRGBO(104, 184, 221, 1),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(40))),
+            ),
+            onPressed: () {},
+          ),
+        ])));
   }
 }
